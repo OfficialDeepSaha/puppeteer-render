f0b03f4c56a1bc7aa1443e1ca1529675df80bc92
Deep Saha <115029656+OfficialDeepSaha@users.noreply.github.com>
2024-11-20 09:20:15 +0530

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,6 +17,7 @@ const scrapeLogic = async (res) => {
   });
   try {
     const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36')
 
     await page.goto("https://developer.chrome.com/");
 
